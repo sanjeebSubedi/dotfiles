8d373a8e984327d27941b401dbd5e9724c8445e1
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2026-01-26 20:51:28 -0600

    minor changes

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,6 +12,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.fillchars = { eob = " " }
 
 -- Search
 vim.opt.ignorecase = true
@@ -23,3 +24,5 @@ vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.undofile = true
 vim.opt.scrolloff = 8
+
+vim.opt.cmdheight = 0
