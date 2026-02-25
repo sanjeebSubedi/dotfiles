79c2e729e8f59ad8a7e77beed66b78fb1ecb210b
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2026-02-24 21:25:11 -0600

    initial linter setup

---
+++ .config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,30 @@
+return {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local lint = require("lint")
+
+        lint.linters_by_ft = {
+            -- python = { "ruff" },
+            javascript = { "eslint_d" },
+            typescript = { "eslint_d" },
+            markdown = { "markdownlint" },
+            sh = { "shellcheck" },
+            -- c = { "clangtidy" },
+            -- cpp = { "clangtidy" },
+            -- zsh = { "shellcheck" },
+        }
+
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+            group = lint_augroup,
+            callback = function()
+                lint.try_lint()
+            end,
+        })
+
+        vim.keymap.set("n", "<leader>ll", function()
+            lint.try_lint()
+        end, { desc = "Trigger linting for current file" })
+    end,
+}
