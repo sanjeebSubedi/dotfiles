e6b0cc87166f53b3836c6461b2be7ffefc897f65
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2026-02-24 21:24:24 -0600

    add keymaps for diagnostics

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,24 @@
--- Standard Operations
-vim.keymap.set("n", "<leader>w", ":w<CR>")
-vim.keymap.set("n", "<leader>q", ":q<CR>")
-vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
+local map = function(mode, lhs, rhs, desc)
+	vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
+end
+
+-- Standard operations
+map("n", "<leader>w", "<cmd>w<CR>", "Write file")
+map("n", "<leader>q", "<cmd>q<CR>", "Quit window")
+map("n", "<Esc>", "<cmd>nohlsearch<CR>", "Clear search highlight")
 
 -- Window Navigation (Ctrl+h/j/k/l)
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+map("n", "<C-h>", "<C-w>h", "Window left")
+map("n", "<C-j>", "<C-w>j", "Window down")
+map("n", "<C-k>", "<C-w>k", "Window up")
+map("n", "<C-l>", "<C-w>l", "Window right")
+
+map("n", "<leader>td", function()
+	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, "Toggle Diagnostics")
+map("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic")
+map("n", "[d", vim.diagnostic.goto_prev, "Previous Diagnostic")
+map("n", "gl", vim.diagnostic.open_float, "Show Diagnostic Line")
+
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
