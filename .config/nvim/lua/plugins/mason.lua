73e775cd403e574271de9a96fd67ec5cd74375b0
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2026-02-24 21:26:16 -0600

    add mason for LSP, linter, formatter

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,22 @@
+return {
+    "mason-org/mason.nvim",
+    dependencies = {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-tool-installer").setup({
+            ensure_installed = {
+                "stylua",
+                "prettier",
+                "ruff",
+                "clang-format",
+                "shfmt",
+                "eslint_d",
+                "shellcheck",
+                "jsonlint",
+                "markdownlint", 
+            },
+        })
+    end,
+}
