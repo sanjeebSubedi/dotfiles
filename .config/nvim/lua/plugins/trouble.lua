8a6aab01b7f3b6fd5ac2576db4195366ea5991f9
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2026-02-24 21:22:04 -0600

    initial trouble =.nvim config

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		-- Your standard configuration
+	},
+	keys = {
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+	},
+}
